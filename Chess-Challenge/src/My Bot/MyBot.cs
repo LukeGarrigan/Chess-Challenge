b6f3c76918acf3668b1bc910c6240dc17a4f8446
Luke <lukegarrigan8@gmail.com>
2023-07-22 17:04:50 +0100

    started minimax

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Data;
+using System.Linq;
+using System.Runtime.InteropServices;
 using ChessChallenge.API;
 
 public class MyBot : IChessBot
@@ -6,55 +9,96 @@ public class MyBot : IChessBot
     public Move Think(Board board, Timer timer)
     {
         Move[] allMoves = board.GetLegalMoves();
-        int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
         // Pick a random move to play if nothing better is found
         Random rng = new();
         Move moveToPlay = allMoves[rng.Next(allMoves.Length)];
-        int highestValueCapture = 0;
 
-        foreach (Move move in allMoves)
+        var best = Int32.MinValue;
+        
+        foreach (var move in allMoves)
         {
-            // Always play checkmate in one
-            if (MoveIsCheckmate(board, move))
-            {
-                moveToPlay = move;
-                break;
-            }
-
-            // Find highest value capture
-            Piece capturedPiece = board.GetPiece(move.TargetSquare);
-            int capturedPieceValue = pieceValues[(int)capturedPiece.PieceType];
-
             board.MakeMove(move);
-            var opponentMoves = board.GetLegalMoves();
-            var opponentBestMove = 0;
-            foreach (var opponentMove in opponentMoves)
-            {
-                if (MoveIsCheckmate(board, opponentMove))
-                {
-                    opponentBestMove = 100000;
-                }
-                Piece myCapturedPiece = board.GetPiece(opponentMove.TargetSquare);
-                int myCapturedPieceValue = pieceValues[(int)myCapturedPiece.PieceType];
 
-                if (myCapturedPieceValue > opponentBestMove)
-                {
-                    opponentBestMove = myCapturedPieceValue;
-                }
-            }
-
-            capturedPieceValue -= opponentBestMove;
-            board.UndoMove(move);
-            
-            if (capturedPieceValue > highestValueCapture)
+            var moveScore = MiniMax(board, 2, true);
+            if (moveScore >= best)
             {
+                best = moveScore;
                 moveToPlay = move;
-                highestValueCapture = capturedPieceValue;
             }
+            
+            board.UndoMove(move);
         }
 
         return moveToPlay;
     }
+
+
+    public int MiniMax(Board board, int depth, bool maximizingPlayer)
+    {
+        if (depth == 0)
+        {
+            return EvaluateBoard(board, board.IsWhiteToMove);
+        }
+
+        if (maximizingPlayer)
+        {
+            var value = -Int32.MaxValue;
+            var moves = board.GetLegalMoves();
+
+            foreach (var move in moves)
+            {
+                board.MakeMove(move);
+                value = Math.Max(value, MiniMax(board, depth - 1, false));
+                board.UndoMove(move);
+            }
+
+            return value;
+        }
+        else
+        {
+            var value = Int32.MaxValue;
+            var moves = board.GetLegalMoves();
+            foreach (var move in moves)
+            {
+                board.MakeMove(move);
+                value = Math.Min(value, MiniMax(board, depth - 1, true));
+                board.UndoMove(move);
+            }
+
+            return value;
+        }
+    }
+
+    private int EvaluateBoard(Board board, bool isWhite)
+    {       
+        int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
+        var pieceLists = board.GetAllPieceLists();
+        var whites = 0;
+        var blacks = 0;
+        foreach (var pieceList in pieceLists)
+        {
+            var pieceValue = pieceValues[(int)pieceList.TypeOfPieceInList];
+            if (pieceList.IsWhitePieceList)
+            {
+                whites += pieceList.Count() * pieceValue;
+            }
+            else
+            {
+                blacks += pieceList.Count() * pieceValue;
+            }
+        }
+
+        
+        if(isWhite)
+        {
+            return whites - blacks;
+        }
+        else
+        {
+            return blacks - whites;
+        }
+    }
+
 
     // Test if this move gives checkmate
     bool MoveIsCheckmate(Board board, Move move)
